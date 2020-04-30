50b099b145df18a3d08e0718732e84ed5f73a956
William ANGER <coding@ideasmashup.com>
2020-04-30 23:06:26 +0200

    feat: guessed value checks

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,12 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let mut rng = rand::thread_rng();
-    let secret: u8 = rng.gen_range(0, 101);
+    let secret: u16 = rng.gen_range(0, 101);
     
     println!("The secret is {}", secret);
 
@@ -17,6 +18,19 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+    let guess: u16 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess {
+        0..=100 => {/* guess in expected range, do nothing */},
+        _ => println!("You must pick a number between 0 and 100!"),
+    }
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Greater => println!("Too high!"),
+        Ordering::Equal => println!("GG! Well played!"),
+    }
 
 }
