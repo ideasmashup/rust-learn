a70b6db76ee52350468117dd9775f7a878c5b762
William ANGER <coding@ideasmashup.com>
2020-04-30 20:41:51 +0200

    feat: random number (different from book)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let mut rng = rand::thread_rng();
+    let secret: u8 = rng.gen_range(0, 101);
+    
+    println!("The secret is {}", secret);
 
     println!("Please input your guess.");
 
