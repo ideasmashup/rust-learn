8f8a488804a372a3065c532284faf3a5fc02eef8
William ANGER <coding@ideasmashup.com>
2020-05-02 20:45:11 +0200

    feat: expressions examples and gotchas

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -10,6 +10,43 @@ fn main() {
 
     local_function();
 
+    // expressions experiments
+    let value = "Wednesday";
+    println!("value = {}", value);
+
+    let value = "Shadow";
+    println!("value = {}", value);
+
+    let value = concat!("Shadow", " ", "Moon");
+    println!("value = {}", value);
+
+    let value = concat!("Shadow", " ", concat!("Moon"));
+    println!("value = {}", value);
+
+    let value = format!("Shadow {}", "Moon");
+    println!("value = {}", value);
+
+    let value = 1;
+    let value = {
+        println!("one plus ?");
+        value + 1
+    };
+    println!("value = {:?}", value);
+
+    // BE CAREFUL
+    // works!
+    let x = {
+        let y = 5;
+        y + 5
+    };
+    println!("value = {}", x);
+
+    // doesn't work the same!
+    let x = {
+        let y = 5;
+        y + 5; // trailing ';' here messes up the expression value of whole {...} scope
+    };
+    println!("value = {:?}", x);
 }
 
 fn my_function() {
