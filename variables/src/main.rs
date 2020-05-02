9ce4047075aa9e216296779f657e86606cd037c0
William ANGER <coding@ideasmashup.com>
2020-05-02 02:44:45 +0200

    feat: variables, mutability samples

---
+++ variables/src/main.rs
@@ -0,0 +1,17 @@
+use std:io;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    let x = x; // shadows the variable to become unmodifiable by others
+
+    // shadowing reduces variables names variants for processing/transforms
+    let age_str = "28";
+    let age_num = age_str.parse();
+
+    // becomes...
+    let age = "28"
+    let age = age.parse();
+}
