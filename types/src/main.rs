33015b1c8529c806c48997540b8f3fd39cf74982
William ANGER <coding@ideasmashup.com>
2020-05-02 11:26:54 +0200

    feat|clean: added more declaration syntaxes and int types

--- types/src/main.rs
+++ types/src/main.rs
@@ -31,7 +31,10 @@ fn main() {
     let int = 255u8;
     println!("Value: {}", int);
 
-    let int: u8 = 255;
+    let int: u8 = 255_u8;        // ugly but works
+    println!("Value: {}", int);
+
+    let int: u8 = 2_5_5_u8;      // even uglier but works too ;)
     println!("Value: {}", int);
 
     let min = u8::MIN;
@@ -41,16 +44,29 @@ fn main() {
 
     // ------
 
-    let int = 2048;
-    println!("Value: {}", int);
-
-    let int = 2_048;
-    println!("Value: {}", int);
-
     let int = 2048i16;
     println!("Value: {}", int);
 
+    let int = 2_048i16;
+    println!("Value: {}", int);
+
     let int: i16 = 2048;
+    println!("Value: {}", int);
+
+    let min = i16::MIN;
+    let max = i16::MAX;
+
+    println!("Specs: i16 {} bytes from {} to {}\n", mem::size_of_val(&int), min, max);
+    
+    // ------
+
+    let int = 65535u16;
+    println!("Value: {}", int);
+
+    let int = 65_535u16;
+    println!("Value: {}", int);
+
+    let int: u16 = 65535;
     println!("Value: {}", int);
 
     let min = u16::MIN;
@@ -60,18 +76,20 @@ fn main() {
     
     // ------
 
-    let int = 65535;
+    let int = 1_234_567_890;
     println!("Value: {}", int);
 
-    let int = 65_535;
+    let int = 1234567890isize;
     println!("Value: {}", int);
 
-    let int = 65535u16;
+    let int = 1_234_567_890isize;
     println!("Value: {}", int);
 
-    let int: u16 = 65535;
+    let int: isize = 1234567890;
     println!("Value: {}", int);
 
+    let min = isize::MIN;
+    let max = isize::MAX;
 
     println!("Specs: isize {} bytes from {} to {}\n", mem::size_of_val(&int), min, max);
     
@@ -83,11 +101,14 @@ fn main() {
     let int = 1_234_567_890;
     println!("Value: {}", int);
 
-    let int: isize = 1234567890;
+    let int = 1234567890usize;
     println!("Value: {}", int);
 
-    let min = isize::min_value();
-    let max = isize::max_value();
+    let int: usize = 1234567890;
+    println!("Value: {}", int);
+
+    let min = usize::MIN;
+    let max = usize::MAX;
 
     println!("Specs: usize {} bytes from {} to {}\n", mem::size_of_val(&int), min, max);
     
@@ -99,14 +120,36 @@ fn main() {
     let int = 1_234_567_890;
     println!("Value: {}", int);
 
+    let int = 1234567890i64;
+    println!("Value: {}", int);
+
+    let int = 1_234_567_890i64;
+    println!("Value: {}", int);
+
+    let int: i64 = 1234567890;
+    println!("Value: {}", int);
+
+    let min = i64::MIN;
+    let max = i64::MAX;
+
+    println!("Specs: i64 {} bytes from {} to {}\n", mem::size_of_val(&int), min, max);
+    
+    // ------
+
     let int = 1234567890u64;
     println!("Value: {}", int);
 
-    let int: usize = 1234567890;
+    let int = 1_234_567_890u64;
     println!("Value: {}", int);
 
-    let min = usize::MIN;
-    let max = usize::MAX;
+    let int: u64 = 1234567890;
+    println!("Value: {}", int);
+
+    let int: u64 = 1_234_567_890;
+    println!("Value: {}", int);
+
+    let min = u64::MIN;
+    let max = u64::MAX;
 
     println!("Specs: u64 {} bytes from {} to {}\n", mem::size_of_val(&int), min, max);
     
