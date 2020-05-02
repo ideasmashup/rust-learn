c9ad335f74e74e13a189f67172df4f287332d181
William ANGER <coding@ideasmashup.com>
2020-05-02 12:35:36 +0200

    clean: variables names no longer type-related
    Change made to better illustrate type inference by compiler and usefulness of explicit type declarations

--- types/src/main.rs
+++ types/src/main.rs
@@ -15,15 +15,15 @@ fn main() {
     
     println!("--- Integers\n");
 
-    let int = 0;
-    println!("Value: {}", int);
-    println!("Specs: {} {} bytes\n", type_of(int), mem::size_of_val(&int));
+    let value = 0;
+    println!("Value: {}", value);
+    println!("Specs: {} {} bytes\n", type_of(value), mem::size_of_val(&value));
 
-    let int = 127i8;
-    println!("Value: {}", int);
+    let value = 127i8;
+    println!("Value: {}", value);
 
-    let int: i8 = 127;
-    println!("Value: {}", int);
+    let value: i8 = 127;
+    println!("Value: {}", value);
 
     let min = i8::MIN;
     let max = i8::MAX;
@@ -32,37 +32,37 @@ fn main() {
        let min = i8::min_value();
        let max = i8::max_value();
 
-       // they work too but don't exist for float types (dunno why)
+       // they work too but don't exist for value types (dunno why)
     */
 
-    println!("Specs: i8 {} byte from {} to {}", mem::size_of_val(&int), min, max);
+    println!("Specs: i8 {} byte from {} to {}", mem::size_of_val(&value), min, max);
 
     // ------
 
-    let int = 255u8;
-    println!("Value: {}", int);
+    let value = 255u8;
+    println!("Value: {}", value);
 
-    let int: u8 = 255_u8;        // ugly but works
-    println!("Value: {}", int);
+    let value: u8 = 255_u8;        // ugly but works
+    println!("Value: {}", value);
 
-    let int: u8 = 2_5_5_u8;      // even uglier but works too ;)
-    println!("Value: {}", int);
+    let value: u8 = 2_5_5_u8;      // even uglier but works too ;)
+    println!("Value: {}", value);
 
     let min = u8::MIN;
     let max = u8::MAX;
 
-    println!("Specs: u8 {} byte from {} to {}\n", mem::size_of_val(&int), min, max);
+    println!("Specs: u8 {} byte from {} to {}\n", mem::size_of_val(&value), min, max);
 
     // ------
 
-    let int = 2048i16;
-    println!("Value: {}", int);
+    let value = 2048i16;
+    println!("Value: {}", value);
 
-    let int = 2_048i16;
-    println!("Value: {}", int);
+    let value = 2_048i16;
+    println!("Value: {}", value);
 
-    let int: i16 = 2048;
-    println!("Value: {}", int);
+    let value: i16 = 2048;
+    println!("Value: {}", value);
 
     let min = i16::MIN;
     let max = i16::MAX;
@@ -71,14 +71,14 @@ fn main() {
     
     // ------
 
-    let int = 65535u16;
-    println!("Value: {}", int);
+    let value = 65535u16;
+    println!("Value: {}", value);
 
-    let int = 65_535u16;
-    println!("Value: {}", int);
+    let value = 65_535u16;
+    println!("Value: {}", value);
 
-    let int: u16 = 65535;
-    println!("Value: {}", int);
+    let value: u16 = 65535;
+    println!("Value: {}", value);
 
     let min = u16::MIN;
     let max = u16::MAX;
@@ -87,17 +87,17 @@ fn main() {
     
     // ------
 
-    let int = 1_234_567_890;
-    println!("Value: {}", int);
+    let value = 1_234_567_890;
+    println!("Value: {}", value);
 
-    let int = 1234567890isize;
-    println!("Value: {}", int);
+    let value = 1234567890isize;
+    println!("Value: {}", value);
 
-    let int = 1_234_567_890isize;
-    println!("Value: {}", int);
+    let value = 1_234_567_890isize;
+    println!("Value: {}", value);
 
-    let int: isize = 1234567890;
-    println!("Value: {}", int);
+    let value: isize = 1234567890;
+    println!("Value: {}", value);
 
     let min = isize::MIN;
     let max = isize::MAX;
@@ -106,17 +106,17 @@ fn main() {
     
     // ------
 
-    let int = 1234567890;
-    println!("Value: {}", int);
+    let value = 1234567890;
+    println!("Value: {}", value);
 
-    let int = 1_234_567_890;
-    println!("Value: {}", int);
+    let value = 1_234_567_890;
+    println!("Value: {}", value);
 
-    let int = 1234567890usize;
-    println!("Value: {}", int);
+    let value = 1234567890usize;
+    println!("Value: {}", value);
 
-    let int: usize = 1234567890;
-    println!("Value: {}", int);
+    let value: usize = 1234567890;
+    println!("Value: {}", value);
 
     let min = usize::MIN;
     let max = usize::MAX;
@@ -125,20 +125,20 @@ fn main() {
     
     // ------
 
-    let int = 1234567890;
-    println!("Value: {}", int);
+    let value = 1234567890;
+    println!("Value: {}", value);
 
-    let int = 1_234_567_890;
-    println!("Value: {}", int);
+    let value = 1_234_567_890;
+    println!("Value: {}", value);
 
-    let int = 1234567890i64;
-    println!("Value: {}", int);
+    let value = 1234567890i64;
+    println!("Value: {}", value);
 
-    let int = 1_234_567_890i64;
-    println!("Value: {}", int);
+    let value = 1_234_567_890i64;
+    println!("Value: {}", value);
 
-    let int: i64 = 1234567890;
-    println!("Value: {}", int);
+    let value: i64 = 1234567890;
+    println!("Value: {}", value);
 
     let min = i64::MIN;
     let max = i64::MAX;
@@ -147,17 +147,17 @@ fn main() {
     
     // ------
 
-    let int = 1234567890u64;
-    println!("Value: {}", int);
+    let value = 1234567890u64;
+    println!("Value: {}", value);
 
-    let int = 1_234_567_890u64;
-    println!("Value: {}", int);
+    let value = 1_234_567_890u64;
+    println!("Value: {}", value);
 
-    let int: u64 = 1234567890;
-    println!("Value: {}", int);
+    let value: u64 = 1234567890;
+    println!("Value: {}", value);
 
-    let int: u64 = 1_234_567_890;
-    println!("Value: {}", int);
+    let value: u64 = 1_234_567_890;
+    println!("Value: {}", value);
 
     let min = u64::MIN;
     let max = u64::MAX;
@@ -166,17 +166,17 @@ fn main() {
     
     // ------
 
-    let int: i128 = 170141183460469231731687303715884105727;
-    println!("Value: {}", int);
+    let value: i128 = 170141183460469231731687303715884105727;
+    println!("Value: {}", value);
 
-    let int: i128 = 170_141_183_460_469_231_731_687_303_715_884_105_727;
-    println!("Value: {}", int);
+    let value: i128 = 170_141_183_460_469_231_731_687_303_715_884_105_727;
+    println!("Value: {}", value);
 
-    let int = 170141183460469231731687303715884105727i128;
-    println!("Value: {}", int);
+    let value = 170141183460469231731687303715884105727i128;
+    println!("Value: {}", value);
 
-    let int = 170_141_183_460_469_231_731_687_303_715_884_105_727i128;
-    println!("Value: {}", int);
+    let value = 170_141_183_460_469_231_731_687_303_715_884_105_727i128;
+    println!("Value: {}", value);
 
     let min = i128::MIN;
     let max = i128::MAX;
@@ -186,70 +186,70 @@ fn main() {
     // -----
 
     // FLOATS
-    // floats declarations
+    // values declarations
     
     println!("--- Floats\n");
 
-    let float = 1.0;
-    println!("Value: {}", float);
-    println!("Specs: {} {} bytes\n", type_of(float), mem::size_of_val(&float));
+    let value = 1.0;
+    println!("Value: {}", value);
+    println!("Specs: {} {} bytes\n", type_of(value), mem::size_of_val(&value));
 
     // -----
     
-    let float: f32 = 1.234_567_89;
-    println!("Value: {}", float);
+    let value: f32 = 1.234_567_89;
+    println!("Value: {}", value);
 
-    let float: f32 = 1.234_567_89;
-    println!("Value: {}", float);
+    let value: f32 = 1.234_567_89;
+    println!("Value: {}", value);
 
-    let float = 1.23456789f32;
-    println!("Value: {}", float);
+    let value = 1.23456789f32;
+    println!("Value: {}", value);
 
-    let float = 1.234_567_89f32;
-    println!("Value: {}", float);
+    let value = 1.234_567_89f32;
+    println!("Value: {}", value);
 
     let min = f32::MIN;
     let max = f32::MAX;
 
-    println!("Specs: f32 {} bytes from {} to {}\n", mem::size_of_val(&float), min, max);
+    println!("Specs: f32 {} bytes from {} to {}\n", mem::size_of_val(&value), min, max);
 
     // -----
 
-    let float: f64 = 1.23456789;
-    println!("Value: {}", float);
+    let value: f64 = 1.23456789;
+    println!("Value: {}", value);
 
-    let float: f64 = 1.234_567_89;
-    println!("Value: {}", float);
+    let value: f64 = 1.234_567_89;
+    println!("Value: {}", value);
 
-    let float = 1.23456789f64;
-    println!("Value: {}", float);
+    let value = 1.23456789f64;
+    println!("Value: {}", value);
 
-    let float = 1.234_567_89f64;
-    println!("Value: {}", float);
+    let value = 1.234_567_89f64;
+    println!("Value: {}", value);
 
     let min = f64::MIN;
     let max = f64::MAX;
 
-    println!("Specs: f64 {} bytes from {} to {}\n", mem::size_of_val(&float), min, max);
+    println!("Specs: f64 {} bytes from {} to {}\n", mem::size_of_val(&value), min, max);
     
     // -----
 
     // BOOLEANS
-    // boolean declarations
+    // value declarations
     
     println!("--- Booleans\n");
 
-    let boolean = true;
-    println!("Value: {}", boolean);
-    println!("Specs: {} {} bytes\n", type_of(boolean), mem::size_of_val(&boolean));
+    let value = true;
+    println!("Value: {}", value);
+    println!("Specs: {} {} bytes\n", type_of(value), mem::size_of_val(&value));
 
-    let boolean = false;
-    println!("Value: {}", boolean);
-    println!("Specs: {} {} bytes\n", type_of(boolean), mem::size_of_val(&boolean));
+    let value = false;
+    println!("Value: {}", value);
+    println!("Specs: {} {} bytes\n", type_of(value), mem::size_of_val(&value));
 
-    let boolean: bool = true || false && true;
-    println!("Value: {}", boolean);
-    println!("Specs: bool {} bytes\n", mem::size_of_val(&boolean));
+    let value: bool = true || false && true;
+    println!("Value: {}", value);
+    println!("Specs: bool {} bytes\n", mem::size_of_val(&value));
 
     // -----
 }
