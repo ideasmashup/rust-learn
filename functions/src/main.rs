767c6bad583e1358717f93b48b2fd786b4e424b8
William ANGER <coding@ideasmashup.com>
2020-05-02 20:44:48 +0200

    feat: functions and expressions examples

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    println!("Hello, world!");
-
+    // functions experiments
     my_function();
+
+    f2(42, "meaning of life");
+
+    fn local_function() {
+        println!("Can this work ?");
+    }
+
+    local_function();
+
 }
 
 fn my_function() {
     println!("Hello, nobody.");
+}
+
+fn f2(arg1: i32, arg2: &str) {
+    println!("Printing {} and {}", arg1, arg2);
 }
